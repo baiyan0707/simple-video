70ab0a47bd2252aaddd5605a5b3c51f5076a2208
bai <baiyan@qinglian.com>
2020-03-16 16:13:42 +0800

    Revert "增加 根据openid获取微信用户信息 接口 增加 公共异常类捕获"
    This reverts commit 9e767e43

---
+++ simple-video-web/src/main/java/com/simple/Application.java
@@ -0,0 +1,53 @@
+package com.simple;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * @author bai
+ * @Description
+ * @Date 2020/3/10 12:20 PM
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan(basePackages = {"com.simple.dao.mapper"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    private final static String DATASOURCE_PREIFX = "spring.datasource";
+
+    @Bean
+    @ConfigurationProperties(prefix = DATASOURCE_PREIFX)
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:/mapper/*.xml"));
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
--- simple-video-web/src/main/java/com/simple/web/http/SimpleVideoApiImpl.java
+++ simple-video-web/src/main/java/com/simple/web/http/SimpleVideoApiImpl.java
@@ -3,7 +3,6 @@ package com.simple.web.http;
 import com.simple.api.ISimpleVideoApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,10 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SimpleVideoApiImpl implements ISimpleVideoApi {
 
-        @Override
-        @PostMapping("/api/test")
-        @ResponseBody
-        public String test(String str) {
-            return str;
-        }
+    @PostMapping("/apis/test")
+    @Override
+    public String test(String str) {
+        return str;
     }
+}
